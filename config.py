0caf8d56c7529db8b9e6af6ff5c8aad5c3fc2ea4
Eran <eranperlman@gmail.com>
2020-11-22 02:43:09 +0200

    added Command line interface

--- config.py
+++ config.py
@@ -22,6 +22,8 @@ WORLDWIDE_CASES_HTML = "Coronavirus Cases:</h1>\n<div class=\"maincounter-number
 
 # Update time list - fetch data at the time items in the list
 UPDATE_TIME = ['08:00:00', '16:00:00', '00:00:00']
+# Countries to fetch - empty list: fetch all countries
+COUNTRIES_FETCH = []
 
 # Error message printed when failing to fetch data
 ERR_MSG_FETCH = 'Failed to fetch data. Check HTML code'
@@ -29,3 +31,8 @@ ERR_MSG_FETCH = 'Failed to fetch data. Check HTML code'
 # Error messages printed when failing to open a file and writing to a file
 ERR_FILE_OPEN = 'Failed to open a file!'
 ERR_FILE_WRITE = 'Failed to write to a file!'
+
+# Args constants
+REQUIRED_NUM_OF_ARGS = 3
+ARG_UPDATES_FILE = 1
+ARG_COUNTRIES_FILE = 2
